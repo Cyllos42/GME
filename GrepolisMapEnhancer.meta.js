ef7363debe9f4c1d761921aeca1692374d2487fa
Cyllos42 <cyllos@protonmail.com>
2017-10-09 21:06:12 +0200

    Change update habits

---
+++ GrepolisMapEnhancer.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Grepolis Map Enhancer
+// @author       Cyllos
+// @description  Grepolis Map Enhancer by Cyllos
+// @include      http://*.grepolis.com/game/*
+// @include      https://*.grepolis.com/game/*
+// @exclude      view-source://*
+// @exclude      https://classic.grepolis.com/game/*
+// @updateURL    
+https://github.com/Cyllos42/GME/raw/master/GrepolisMapEnhancer.meta.js
+// @downloadURL  
+https://github.com/Cyllos42/GME/raw/master/GrepolisMapEnhancer.user.js
+// @version      1.3.1
+// @grant        none
+// ==/UserScript==
