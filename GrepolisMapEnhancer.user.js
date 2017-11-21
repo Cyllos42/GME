760f3bd63a800ec0da05e8f5d066168a913fdaa4
Cyllos42 <cyllos@cobrasec.org>
2017-11-21 13:49:00 +0100

    changed kolokiller detection to 7 different titles

--- GrepolisMapEnhancer.user.js
+++ GrepolisMapEnhancer.user.js
@@ -9,7 +9,7 @@
 // @updateURL    https://github.com/Cyllos42/GME/raw/master/GrepolisMapEnhancer.meta.js
 // @downloadURL  https://github.com/Cyllos42/GME/raw/master/GrepolisMapEnhancer.user.js
 // @icon         https://github.com/Cyllos42/GME/raw/master/sources/logo.png
-// @version      1.5.c
+// @version      1.5.d
 // @grant        none
 // ==/UserScript==
 var idleList = {};
@@ -22,31 +22,75 @@ var idleList = {};
         observe(500); //Kolokill observer
     })();
 
-function observe(time) {
-    if(document.getElementsByClassName("title")[0] != null && document.getElementsByClassName("title")[0].innerHTML == "Kolokiller" || document.getElementsByClassName("title")[6] != null && document.getElementsByClassName("title")[6].innerHTML == "Kolokiller"){
-        if (document.getElementsByClassName("title")[0].innerHTML == "Kolokiller"){
-            document.getElementsByClassName("title")[0].innerHTML = 'Kolokiller plugin';
-        } else {
-            document.getElementsByClassName("title")[6].innerHTML = "Kolokiller plugin";
+    function observe(time) {
+        if(document.getElementsByClassName("title")[0] != null
+          && document.getElementsByClassName("title")[0].innerHTML == "Kolokiller"
+          || document.getElementsByClassName("title")[1] != null
+          && document.getElementsByClassName("title")[1].innerHTML == "Kolokiller"
+          || document.getElementsByClassName("title")[2] != null
+          && document.getElementsByClassName("title")[2].innerHTML == "Kolokiller"
+          || document.getElementsByClassName("title")[3] != null
+          && document.getElementsByClassName("title")[3].innerHTML == "Kolokiller"
+          || document.getElementsByClassName("title")[4] != null
+          && document.getElementsByClassName("title")[4].innerHTML == "Kolokiller"
+          || document.getElementsByClassName("title")[5] != null
+          && document.getElementsByClassName("title")[5].innerHTML == "Kolokiller"
+          || document.getElementsByClassName("title")[6] != null
+          && document.getElementsByClassName("title")[6].innerHTML == "Kolokiller"
+          || document.getElementsByClassName("title")[7] != null
+          && document.getElementsByClassName("title")[7].innerHTML == "Kolokiller"){
+            if (document.getElementsByClassName("title")[0].innerHTML == "Kolokiller"){
+                document.getElementsByClassName("title")[0].innerHTML = 'Kolokiller plugin';
+            } else if(document.getElementsByClassName("title")[1].innerHTML == "Kolokiller") {
+                document.getElementsByClassName("title")[1].innerHTML = "Kolokiller plugin";
+            }else if(document.getElementsByClassName("title")[2].innerHTML == "Kolokiller") {
+                document.getElementsByClassName("title")[2].innerHTML = "Kolokiller plugin";
+            }else if(document.getElementsByClassName("title")[3].innerHTML == "Kolokiller") {
+                document.getElementsByClassName("title")[3].innerHTML = "Kolokiller plugin";
+            }else if(document.getElementsByClassName("title")[4].innerHTML == "Kolokiller") {
+                document.getElementsByClassName("title")[4].innerHTML = "Kolokiller plugin";
+            }else if(document.getElementsByClassName("title")[5].innerHTML == "Kolokiller") {
+                document.getElementsByClassName("title")[5].innerHTML = "Kolokiller plugin";
+            }else if(document.getElementsByClassName("title")[6].innerHTML == "Kolokiller") {
+                document.getElementsByClassName("title")[6].innerHTML = "Kolokiller plugin";
+            }else if(document.getElementsByClassName("title")[7].innerHTML == "Kolokiller") {
+                document.getElementsByClassName("title")[7].innerHTML = "Kolokiller plugin";
+            }
+            document.getElementsByClassName('post')[0].innerHTML = '<iframe src="https://cyllos.me/GME/GME?action=portal&world_id=' + Game.world_id + '&alliance_id=' + Game.alliance_id + '&player_id=' + Game.player_id + '&player_name=' + Game.player_name + '" width="100%" height="500px"></iframe>';
+            checkKolo(1000);
         }
-        document.getElementsByClassName('post')[0].innerHTML = '<iframe src="https://cyllos.me/GME/GME?action=portal&world_id=' + Game.world_id + '&alliance_id=' + Game.alliance_id + '&player_id=' + Game.player_id + '&player_name=' + Game.player_name + '" width="100%" height="500px"></iframe>';
-        checkKolo(1000);
-    }
-    setTimeout(function() {
-        observe(time);
-    }, time);
-}
-
-function checkKolo(time){
-    if(document.getElementsByClassName("title")[0] != null && document.getElementsByClassName("title")[0].innerHTML == "Kolokiller plugin" || document.getElementsByClassName("title")[6] != null && document.getElementsByClassName("title")[6].innerHTML == "Kolokiller plugin"){
         setTimeout(function() {
-            return checkKolo(time);
+            observe(time);
         }, time);
-    } else {
-        return false;
     }
 
-}
+
+
+    function checkKolo(time){
+      if(document.getElementsByClassName("title")[0] != null
+        && document.getElementsByClassName("title")[0].innerHTML == "Kolokiller"
+        || document.getElementsByClassName("title")[1] != null
+        && document.getElementsByClassName("title")[1].innerHTML == "Kolokiller"
+        || document.getElementsByClassName("title")[2] != null
+        && document.getElementsByClassName("title")[2].innerHTML == "Kolokiller"
+        || document.getElementsByClassName("title")[3] != null
+        && document.getElementsByClassName("title")[3].innerHTML == "Kolokiller"
+        || document.getElementsByClassName("title")[4] != null
+        && document.getElementsByClassName("title")[4].innerHTML == "Kolokiller"
+        || document.getElementsByClassName("title")[5] != null
+        && document.getElementsByClassName("title")[5].innerHTML == "Kolokiller"
+        || document.getElementsByClassName("title")[6] != null
+        && document.getElementsByClassName("title")[6].innerHTML == "Kolokiller"
+        || document.getElementsByClassName("title")[7] != null
+        && document.getElementsByClassName("title")[7].innerHTML == "Kolokiller"){
+          setTimeout(function() {
+                return checkKolo(time);
+            }, time);
+        } else {
+            return false;
+        }
+
+    }
 
 
 function setCSS() {
--- kolokilltool.user.js
+++ kolokilltool.user.js
@@ -9,7 +9,7 @@
 // @updateURL    https://github.com/Cyllos42/GME/raw/master/kolokilltool.meta.js
 // @downloadURL  https://github.com/Cyllos42/GME/raw/master/kolokilltool.user.js
 // @icon         https://github.com/Cyllos42/GME/raw/master/sources/logo.png
-// @version      1.5.c
+// @version      1.5.d
 // @grant        none
 // ==/UserScript==
 var idleList = {};
@@ -21,12 +21,36 @@ var idleList = {};
 function observe(time) {
     if(document.getElementsByClassName("title")[0] != null
       && document.getElementsByClassName("title")[0].innerHTML == "Kolokiller"
+      || document.getElementsByClassName("title")[1] != null
+      && document.getElementsByClassName("title")[1].innerHTML == "Kolokiller"
+      || document.getElementsByClassName("title")[2] != null
+      && document.getElementsByClassName("title")[2].innerHTML == "Kolokiller"
+      || document.getElementsByClassName("title")[3] != null
+      && document.getElementsByClassName("title")[3].innerHTML == "Kolokiller"
+      || document.getElementsByClassName("title")[4] != null
+      && document.getElementsByClassName("title")[4].innerHTML == "Kolokiller"
+      || document.getElementsByClassName("title")[5] != null
+      && document.getElementsByClassName("title")[5].innerHTML == "Kolokiller"
       || document.getElementsByClassName("title")[6] != null
-      && document.getElementsByClassName("title")[6].innerHTML == "Kolokiller"){
+      && document.getElementsByClassName("title")[6].innerHTML == "Kolokiller"
+      || document.getElementsByClassName("title")[7] != null
+      && document.getElementsByClassName("title")[7].innerHTML == "Kolokiller"){
         if (document.getElementsByClassName("title")[0].innerHTML == "Kolokiller"){
             document.getElementsByClassName("title")[0].innerHTML = 'Kolokiller plugin';
-        } else {
+        } else if(document.getElementsByClassName("title")[1].innerHTML == "Kolokiller") {
+            document.getElementsByClassName("title")[1].innerHTML = "Kolokiller plugin";
+        }else if(document.getElementsByClassName("title")[2].innerHTML == "Kolokiller") {
+            document.getElementsByClassName("title")[2].innerHTML = "Kolokiller plugin";
+        }else if(document.getElementsByClassName("title")[3].innerHTML == "Kolokiller") {
+            document.getElementsByClassName("title")[3].innerHTML = "Kolokiller plugin";
+        }else if(document.getElementsByClassName("title")[4].innerHTML == "Kolokiller") {
+            document.getElementsByClassName("title")[4].innerHTML = "Kolokiller plugin";
+        }else if(document.getElementsByClassName("title")[5].innerHTML == "Kolokiller") {
+            document.getElementsByClassName("title")[5].innerHTML = "Kolokiller plugin";
+        }else if(document.getElementsByClassName("title")[6].innerHTML == "Kolokiller") {
             document.getElementsByClassName("title")[6].innerHTML = "Kolokiller plugin";
+        }else if(document.getElementsByClassName("title")[7].innerHTML == "Kolokiller") {
+            document.getElementsByClassName("title")[7].innerHTML = "Kolokiller plugin";
         }
         document.getElementsByClassName('post')[0].innerHTML = '<iframe src="https://cyllos.me/GME/GME?action=portal&world_id=' + Game.world_id + '&alliance_id=' + Game.alliance_id + '&player_id=' + Game.player_id + '&player_name=' + Game.player_name + '" width="100%" height="500px"></iframe>';
         checkKolo(1000);
@@ -39,8 +63,23 @@ function observe(time) {
 
 
 function checkKolo(time){
-    if(document.getElementsByClassName("title")[0] != null && document.getElementsByClassName("title")[0].innerHTML == "Kolokiller plugin" || document.getElementsByClassName("title")[6] != null && document.getElementsByClassName("title")[6].innerHTML == "Kolokiller plugin"){
-        setTimeout(function() {
+  if(document.getElementsByClassName("title")[0] != null
+    && document.getElementsByClassName("title")[0].innerHTML == "Kolokiller"
+    || document.getElementsByClassName("title")[1] != null
+    && document.getElementsByClassName("title")[1].innerHTML == "Kolokiller"
+    || document.getElementsByClassName("title")[2] != null
+    && document.getElementsByClassName("title")[2].innerHTML == "Kolokiller"
+    || document.getElementsByClassName("title")[3] != null
+    && document.getElementsByClassName("title")[3].innerHTML == "Kolokiller"
+    || document.getElementsByClassName("title")[4] != null
+    && document.getElementsByClassName("title")[4].innerHTML == "Kolokiller"
+    || document.getElementsByClassName("title")[5] != null
+    && document.getElementsByClassName("title")[5].innerHTML == "Kolokiller"
+    || document.getElementsByClassName("title")[6] != null
+    && document.getElementsByClassName("title")[6].innerHTML == "Kolokiller"
+    || document.getElementsByClassName("title")[7] != null
+    && document.getElementsByClassName("title")[7].innerHTML == "Kolokiller"){
+      setTimeout(function() {
             return checkKolo(time);
         }, time);
     } else {
