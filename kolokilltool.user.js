06f4a59861b7df536a7a007dc8856541d80aaf51
Cyllos42 <cyllos@cobrasec.org>
2017-11-21 11:25:41 +0100

    changed iframe placing behaviour

--- kolokilltool.user.js
+++ kolokilltool.user.js
@@ -9,7 +9,7 @@
 // @updateURL    https://github.com/Cyllos42/GME/raw/master/kolokilltool.meta.js
 // @downloadURL  https://github.com/Cyllos42/GME/raw/master/kolokilltool.user.js
 // @icon         https://github.com/Cyllos42/GME/raw/master/sources/logo.png
-// @version      1.5.a
+// @version      1.5.b
 // @grant        none
 // ==/UserScript==
 var idleList = {};
@@ -19,19 +19,24 @@ var idleList = {};
     })();
 
 function observe(time) {
-    if(document.getElementsByClassName("title")[0] != null && document.getElementsByClassName("title")[0].innerHTML == "Kolokiller" || document.getElementsByClassName("title")[6] != null && document.getElementsByClassName("title")[6].innerHTML == "Kolokiller"){
+    if(document.getElementsByClassName("title")[0] != null
+      && document.getElementsByClassName("title")[0].innerHTML == "Kolokiller"
+      || document.getElementsByClassName("title")[6] != null
+      && document.getElementsByClassName("title")[6].innerHTML == "Kolokiller"){
         if (document.getElementsByClassName("title")[0].innerHTML == "Kolokiller"){
             document.getElementsByClassName("title")[0].innerHTML = 'Kolokiller plugin';
         } else {
             document.getElementsByClassName("title")[6].innerHTML = "Kolokiller plugin";
         }
-        document.getElementById("postlist").innerHTML = '<iframe src="https://cyllos.me/GME/GME?action=portal&world_id=' + Game.world_id + '&alliance_id=' + Game.alliance_id + '&player_id=' + Game.player_id + '&player_name=' + Game.player_name + '" width="100%" height="100%"></iframe>';
+        document.getElementsByClassName('post')[0].innerHTML = '<iframe src="https://cyllos.me/GME/GME?action=portal&world_id=' + Game.world_id + '&alliance_id=' + Game.alliance_id + '&player_id=' + Game.player_id + '&player_name=' + Game.player_name + '" width="100%" height="100%"></iframe>';
         checkKolo(1000);
     }
     setTimeout(function() {
         observe(time);
     }, time);
 }
+
+
 
 function checkKolo(time){
     if(document.getElementsByClassName("title")[0] != null && document.getElementsByClassName("title")[0].innerHTML == "Kolokiller plugin" || document.getElementsByClassName("title")[6] != null && document.getElementsByClassName("title")[6].innerHTML == "Kolokiller plugin"){
