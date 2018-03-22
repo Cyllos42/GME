8d2192224da4a5f16483c49dfc481d1f9a6e1de1
cyllos <cyllos@protonmail.com>
2018-03-22 23:48:05 +0100

    Update GrepolisMapEnhancer.user.js

--- GrepolisMapEnhancer.user.js
+++ GrepolisMapEnhancer.user.js
@@ -9,7 +9,7 @@
 // @updateURL    https://github.com/Cyllos42/GME/raw/master/GrepolisMapEnhancer.meta.js
 // @downloadURL  https://github.com/Cyllos42/GME/raw/master/GrepolisMapEnhancer.user.js
 // @icon         https://github.com/Cyllos42/GME/raw/master/sources/logo_geenkader.png
-// @version      2018.3.21
+// @version      2018.3.22
 // @grant GM_setValue
 // @grant GM_getValue
 // @grant unsafeWindow
